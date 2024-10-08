3399ee0308eada2e822753aa84d5ec83d3726556
Samantha Hernandez <samhercab@gmail.com>
2024-10-07 21:59:43 -0600

    no se regresar en commits

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
