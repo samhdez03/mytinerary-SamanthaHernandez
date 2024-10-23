157747244a85e9cb9677670d7d832f9c002700da
Samantha Hernandez <samhercab@gmail.com>
2024-10-22 18:21:46 -0600

    nuevo

--- src/Pages/Cities.jsx
+++ src/Pages/Cities.jsx
@@ -1,4 +1,22 @@
 function Cities(){
+    useEffect(() => {
+        const ftch = async ()=>{
+          try {
+            let citiesFetch = await fetch("https://valorant-api.com/v1/agents");
+            let datosCities = await citiesFetch.json(); 
+            setCargando(false);
+            datosCities = datosCities.data 
+    
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        ftch()
+        }
+        , []);
+    if (cargando) {
+          return <div><p className="sm:text-md md:text-md text-xl font-fedra text-white">Cargando...</p></div>; // Muestra mensaje de carga mientras se espera la respuesta
+        }
     return(
         <>
             <main className="flex-grow mt-20">
